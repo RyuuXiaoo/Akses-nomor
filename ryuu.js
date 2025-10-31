e4f9b787f88b4c93a33bde7822b4a7cfb6e5ab80
RyuuXiao <164801435+RyuuXiaoo@users.noreply.github.com>
2025-10-31 14:14:02 +0700

    Update ryuu.js

--- ryuu.js
+++ ryuu.js
@@ -1,2 +1,3 @@
 [{"number":"6285929924135","status":"active"},
-{"number":"628983623523","status":"active"}]
+{"number":"628983623523","status":"active"},
+{"number":"6289507143171","status":"active"}]
